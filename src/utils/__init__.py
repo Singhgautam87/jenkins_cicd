6b819c80ecb0cd0985f598198cf1ec70c5cdf51e
gautam singh <gautam901267@gmail.com>
2026-01-22 23:57:28 +0530

    add spark session

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,17 @@
 """Utility functions and helpers."""
 from datetime import datetime
+from pyspark.sql import SparkSession
 
 def parse_run_date(date_str):
     """Parse date string YYYY-MM-DD to datetime"""
     return datetime.strptime(date_str, '%Y-%m-%d')
+
+
+
+def create_spark(app_name="ZoomcarETL"):
+    """Create and return Spark session"""
+    return SparkSession.builder \
+        .appName(app_name) \
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
+        .config("spark.sql.streaming.schemaInference", "true") \
+        .getOrCreate()

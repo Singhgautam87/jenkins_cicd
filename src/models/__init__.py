d0c151f6b1dcd9f76b4af7bce9e8910f7fd46e2f
gautam singh <gautam901267@gmail.com>
2026-01-22 23:23:16 +0530

    add the datetime function

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,6 @@
 """Data models for bookings and customers."""
+from datetime import datetime
+
+def parse_run_date(date_str):
+    """Parse date string YYYY-MM-DD to datetime"""
+    return datetime.strptime(date_str, '%Y-%m-%d')
